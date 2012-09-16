7f498942d2d26de74ff7fe682b2c65a65c0dd4f8
Wes Freeman <freeman.wes@gmail.com>
2012-09-15 22:12:33 -0400

    Added comments to the functions instead of in comments.

--- src/Clojurithms/unionfind.clj
+++ src/Clojurithms/unionfind.clj
@@ -3,24 +3,29 @@
 ; this implements the Weighted Quick Union UF algorithm
 (defrecord unionfind [ids sizes])
 
-(defn with-length [n]
+(defn with-length
+  "creates a union find record with a starting length"
+  [n]
   (let [ids (vec (range n))
         sizes (vec (repeat n 1))]
     (->unionfind ids sizes)))
 
-; finds the root of p
-(defn find-root [uf p]
-   (loop [p p]
-      (if (= p ((:ids uf) p))
-         p
-         (recur ((:ids uf) p)))))
+(defn find-root
+  "finds the root of p"
+  [uf p]
+  (loop [p p]
+    (if (= p ((:ids uf) p))
+      p
+      (recur ((:ids uf) p)))))
 
-; is p connected to q?
-(defn connected? [uf p q]
+(defn connected? 
+  "is p connected to q?"
+  [uf p q]
   (= (find-root uf p) (find-root uf q)))
 
-; connect p and q, returning the result
-(defn union [uf p q]
+(defn union 
+  "connect p and q, returning the result."
+  [uf p q]
   (let [i (find-root uf p) j (find-root uf q)]
     (if (= i j)
         uf
